bdbf6243f513212fe9d0d8aa226d2982f4e0717a
Nerijus Arlauskas <nercury@gmail.com>
2009-07-18 18:03:12 +0300

    Implemented EntityUpgradeActions.cs
    Signed-off-by: Nerijus Arlauskas <nercury@gmail.com>

--- src/fluentNUpgrade/UpgradeGen/EntityUpgradeActions.cs
+++ src/fluentNUpgrade/UpgradeGen/EntityUpgradeActions.cs
@@ -11,7 +11,7 @@ namespace FluentNUpgrade.UpgradeGen
         private Dictionary<string, ColumnUpgradeActions> addedColumns = new Dictionary<string, ColumnUpgradeActions>();
         private Dictionary<string, bool> removedColumns = new Dictionary<string, bool>();
 
-        public EntityUpgradeActions AddColumn<ColumnT>(string name)
+        private ColumnUpgradeActions RegisterColumnAdd(string name)
         {
             ColumnUpgradeActions actions;
             if (addedColumns.TryGetValue(name, out actions))
@@ -20,27 +20,57 @@ namespace FluentNUpgrade.UpgradeGen
             }
             else
             {
-                if (removedColumns.ContainsKey(name))
-                    removedColumns.Remove(name);
+                removedColumns.Remove(name);
 
                 actions = new ColumnUpgradeActions();
                 addedColumns[name] = actions;
             }
+            return actions;
+        }
+
+        private void RegisterColumnRemove(string name)
+        {
+            if (removedColumns.ContainsKey(name))
+            {
+                throw new ColumnAlreadyRemovedException("Column with name [" + name + "] already removed from entity.");
+            }
+            else
+            {
+                addedColumns.Remove(name);
+
+                removedColumns[name] = true; // boolean value does not mean anything here
+            }
+        }
+
+        public EntityUpgradeActions AddColumn<ColumnT>(string name)
+        {
+            RegisterColumnAdd(name);
             return this;
         }
 
         public EntityUpgradeActions AddColumn<ColumnT>(string name, Action<ColumnUpgradeActions> columnSetup)
         {
+            columnSetup(RegisterColumnAdd(name));
             return this;
         }
 
         public EntityUpgradeActions RemoveColumn<ColumnT>(string name)
         {
+            RegisterColumnRemove(name);
             return this;
         }
 
         public EntityUpgradeActions ModifyColumn<ColumnT>(string name, Action<ColumnUpgradeActions> columnSetup)
         {
+            ColumnUpgradeActions actions;
+            if (!addedColumns.TryGetValue(name, out actions))
+            {
+                throw new ColumnNotFoundException("Column with name [" + name + "] was not found in entity.");
+            }
+            else
+            {
+                columnSetup(actions);
+            }
             return this;
         }
     }
---
+++ src/fluentNUpgrade/UpgradeGen/Exceptions/ColumnNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FluentNUpgrade.Exceptions;
+
+namespace FluentNUpgrade.UpgradeGen.Exceptions
+{
+    public class ColumnNotFoundException : NUpgradeException
+    {
+        public ColumnNotFoundException() : base() { }
+        public ColumnNotFoundException(string message) : base(message) { }
+        public ColumnNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
